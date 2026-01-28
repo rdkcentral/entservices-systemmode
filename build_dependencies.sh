6c6f08a2a05aa8961098e7dccac76d23db066c00
preeja33 <preeja_raveendran@comcast.com>
2026-01-28 14:03:39 +0530

    Update build_dependencies.sh

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -10,7 +10,7 @@ cd ${GITHUB_WORKSPACE}
 #1. Install Dependencies and packages
 
 apt update
-apt install -y libcurl4-openssl-dev valgrind lcov clang libsystemd-dev libboost-all-dev libwebsocketpp-dev meson curl libunwind-dev 
+apt install -y libcurl4-openssl-dev valgrind lcov clang libsystemd-dev libboost-all-dev libwebsocketpp-dev meson curl libunwind-dev libdrm-dev
 pip install jsonref
 
 git clone --branch  R4.4.3 https://github.com/rdkcentral/ThunderTools.git
